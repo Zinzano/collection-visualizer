47075fe12e9d78733cefae18b8c3e929fe39e4ad
Zinzano <svalan@gmail.com>
2020-07-01 07:43:54 +0200

    Added searchbar that searches the RAWG video games database

--- src/App.js
+++ src/App.js
@@ -1,13 +1,64 @@
-import React from 'react';
-import doitImage from './resources/images/doit.jpg'
+import React, { useState } from  'react';
 import './App.css';
+import Searchbar from "./components/Searchbar";
+import axios from 'axios';
+
 
 function App() {
+    const [state, setState] = useState({
+        searchQuery: "",
+        results: [],
+        selectedGame: {}
+        }
+    )
+
+   const searchbarSearchHandler = (e) => {
+       if (e.key === "Enter") {
+           axios({
+               method: 'get',
+               headers: {
+                   "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
+                   "x-rapidapi-key": "f88bb6e14fmsh5c7e6d29330655ap1faad0jsn4679c15352d6"
+               },
+               url: "https://rawg-video-games-database.p.rapidapi.com/games",
+               params: {
+                   search: state.searchQuery
+               }
+           }).then(( {data} ) => {
+               let res = data.results;
+
+               setState(prevState => {
+                   return { ...prevState, results: res}
+               })
+
+           });
+       }
+   }
+
+    const searchboxInputHandler= (e) => {
+        let s = e.target.value;
+
+        setState( prevState => {
+            return { ...prevState, searchQuery: s}
+        });
+    }
+
+
   return (
     <div className="App">
-      <h1>Collection visualizer</h1>
-      <h2>Let's learn React and build an app that can visualize a collection from a database</h2>
-      <img src={doitImage} alt="doit" />
+        <header>
+            <h1>Collection visualizer</h1>
+            <h2>Search for a game in the RAWG Video Games Database</h2>
+        </header>
+
+        <main>
+            <Searchbar inputHandler={searchboxInputHandler} searchHandler={searchbarSearchHandler} />
+
+            { state.results.map(r => (
+                <div key={r.id} className="result">{r.name}</div>
+            ))}
+        </main>
+
     </div>
   );
 }
